f78e8c71078ecb3c175e292d840a7cdc71d48de2
Dukat <dukat@coinrank.chat>
2018-12-30 21:38:07 +0200

    store stats in db store

--- tradeiobot/stats.py
+++ tradeiobot/stats.py
@@ -1,28 +1,27 @@
 import time
+from tradeiobot.db import connection
 
-#FIXME these stats are likely off. find out how the telegram bot lib actually runs (i.e. multiple threads?)
 
-
-hits = 0
-users = set()
 running_since = time.time()
 
 def track(handler):
     "decorator for telegram bot handlers"
 
     def wrapper(*args, **kwargs):
-        global hits
-        global users
-        hits += 1
-        users.add(args[1].message.from_user.id)
+        total_hits = int(connection.get("stats", "total_hits") or 0)
+        connection.set("stats", "total_hits", total_hits + 1)
+
+        user = args[1].message.from_user.id
+        user_hits = int(connection.get("user_hits", user) or 0)
+        connection.set("user_hits", user, user_hits + 1)
         return handler(*args, **kwargs)
     return wrapper
 
 def get_hits():
-    return hits
+    return int(connection.get("stats", "total_hits") or 0)
 
 def get_unique_users():
-    return len(users)
+    return len(connection.get_all_as_dict("user_hits"))
 
 def get_uptime():
     return int(time.time() - running_since)
--- tradeiobot/test/test_stats.py
+++ tradeiobot/test/test_stats.py
@@ -1,5 +1,13 @@
-import tradeiobot.stats
 from tradeiobot.util import dotdict
+
+
+import tradeiobot.db
+import tradeiobot.db.stores
+
+# patch connection to use memory store only
+tradeiobot.db.connection = tradeiobot.db.connect([tradeiobot.db.stores.memory])
+
+import tradeiobot.stats
 
 
 def create_dummy_telegram_update(user_id=0):
@@ -30,5 +38,5 @@ def test_track_hits():
 def test_get_unique_users():
     dummy_telegram_handler(None, create_dummy_telegram_update())
     assert tradeiobot.stats.get_unique_users() == 1
-    dummy_telegram_handler(None, create_dummy_telegram_update("abc"))
+    dummy_telegram_handler(None, create_dummy_telegram_update("user2"))
     assert tradeiobot.stats.get_unique_users() == 2
