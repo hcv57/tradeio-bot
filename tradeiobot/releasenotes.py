1cf4dbf2b85d2d1580d16933b1e3d245d3c68e13
Dukat <dukat@coinrank.chat>
2019-01-11 16:36:53 +0200

    Properly compare versions to determine whether to show release notes

--- tradeiobot/releasenotes.py
+++ tradeiobot/releasenotes.py
@@ -1,9 +1,7 @@
 import tradeiobot.bot.handlers
 from tradeiobot.config import VERSION
 from tradeiobot.db import connection
-
-
-current_release = float(VERSION.split(",")[0])
+from distutils.version import LooseVersion
 
 def showonce(handler):
     "decorator for telegram bot handlers"
@@ -11,10 +9,10 @@ def showonce(handler):
     def wrapper(*args, **kwargs):
         global users
         user = args[1].message.from_user.id
-        known_release = float(connection.get("user_release", user) or 0)
-        if current_release > known_release:
+        known_release = connection.get("user_release", user) or "0"
+        if LooseVersion(VERSION) > LooseVersion(known_release):
             _handler = tradeiobot.bot.handlers.releasenotes_handler
-            connection.set("user_release", user, current_release)
+            connection.set("user_release", user, VERSION)
         else:
             _handler = handler
         return _handler(*args, **kwargs)
